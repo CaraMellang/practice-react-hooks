0808325d8e0f31d308576771cff9b50b523bc95f
Mellang <dlwpgh23@naver.com>
2022-06-28 23:27:37 +0900

    문제풀기

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Home = () =>{
-    return(
-        <div>
-            <h1>홈입니다</h1>
-        </div>
-    )
-}
-export default Home
+const Home = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={() => setCount((prev) => prev + 1)}>증가</button>
+      <button
+        onClick={() =>
+          setTimeout(() => {
+            console.log("나 뭐게?", count);
+          }, 5000)
+        }
+      >
+        실행하자
+      </button>
+      <h1>홈입니다</h1>
+    </div>
+  );
+};
+export default Home;
